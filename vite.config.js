d6b59014698c1218a2baef1e868592c55ece9af3
isaac <isaac976abdou@gmail.com>
2026-03-07 02:17:06 +0100

    move files to root

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,1 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-
-export default defineConfig({
-  plugins: [vue()],
-  base: '/testem/',  // ← remplace par le nom exact de ton repo GitHub
-});
+import { defineConfig } from 'vite'; import vue from '@vitejs/plugin-vue'; export default defineConfig({ plugins:[vue()] });
