c1fe78d77757f1079ccccb1accf94aedb6888830
isaac <isaac976abdou@gmail.com>
2026-03-07 01:50:45 +0100

    fix: ajout base vite config

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,7 @@
-import { defineConfig } from 'vite'; import vue from '@vitejs/plugin-vue'; export default defineConfig({ plugins:[vue()] });
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  plugins: [vue()],
+  base: '/testem/',  // ← remplace par le nom exact de ton repo GitHub
+});
